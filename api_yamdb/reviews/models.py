c960310129b299b1d5f706675f83e4a12bb3f0eb
Sergey Shlemin <schlyomin@yandex.ru>
2023-02-20 20:06:36 +0300

    Правки 2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from .validators import validate_year, validate_username
+from .validators import validate_username, validate_year
 
 
 class User(AbstractUser):
