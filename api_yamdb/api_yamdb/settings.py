385faf777430206c2aa88eaf976be94fcd45926d
Sergey Shlemin <schlyomin@yandex.ru>
2023-02-23 18:16:21 +0300

    Final

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='string')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    os.getenv('ALLOWED_HOSTS', default='string')]
 
 INSTALLED_APPS = [
     'django.contrib.auth',
